20f02a64609a19ebcee0934da882c52476f00527
Daniel Castro <daniel.castro@critigen.com>
2019-01-26 16:05:14 -0800

    Add series 2 and some of series 3

--- students/DanielCastro/DanielCastro-A03/list_lab.py
+++ students/DanielCastro/DanielCastro-A03/list_lab.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
-fruits = ['apples', 'pears', 'oranges', 'peaches']
-print(fruits)
-new_fruit = input('Add your favorite fruit: ')
-fruits.append(new_fruit)
-print(fruits)
-choose_fruit = int(input('Pick a number 1 through {}: '.format(len(fruits))))
-print('You chose {}: {}'.format(choose_fruit, fruits[choose_fruit - 1]))
-fruits = [input('Add another fruit: ')] + fruits
-print(fruits)
-fruits.insert(0, input('Add another fruit: '))
-print(fruits)
+fruits = ['Apples', 'Pears', 'Oranges', 'Peaches']
+# print('SERIES 1')
+# print('=' * 40)
+# print(fruits)
+# new_fruit = input('Add your favorite fruit: ')
+# fruits.append(new_fruit)
+# print(fruits)
+# choose_fruit = int(input('Pick a number 1 through {}: '.format(len(fruits))))
+# print('You chose {}: {}'.format(choose_fruit, fruits[choose_fruit - 1]))
+# fruits = [input('Add another fruit: ')] + fruits
+# print(fruits)
+# fruits.insert(0, input('Add another fruit: '))
+# print(fruits)
+# for fruit in fruits:
+#     if fruit[0] == 'P':
+#         print(fruit)
+# print('=' * 40)
 
+# print('SERIES 2')
+# print('=' * 40)
+# print(fruits)
+# fruits.pop(-1)
+# print(fruits)
+# remove_fruit = input('Delete a fruit: ')
+# fruits.remove(remove_fruit)
+# print(fruits)
+# print('=' * 40)
+
+print('SERIES 3')
+print('=' * 40)
 for fruit in fruits:
-    if fruit[0] == 'p':
-        print(fruit)
+    answer = input('Do you like {}? '.format(fruit.lower()))
+    while answer != 'yes' or 'no':
+        print('Please type "yes" or "no"')
+        answer = input('Do you like {}? '.format(fruit.lower()))
