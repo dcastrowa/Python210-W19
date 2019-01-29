558eef4371d81b1f79e5c5481e37d9aa675f8c65
Daniel Castro <daniel.castro@critigen.com>
2019-01-28 17:41:11 -0800

    Add series 3 and complete file

--- students/DanielCastro/DanielCastro-A03/list_lab.py
+++ students/DanielCastro/DanielCastro-A03/list_lab.py
@@ -1,36 +1,47 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
+# Title: List Lab
+# Change Log: (Who,What,When)
+# dcastrowa, created and completed file, 01/28/19
 fruits = ['Apples', 'Pears', 'Oranges', 'Peaches']
-# print('SERIES 1')
-# print('=' * 40)
-# print(fruits)
-# new_fruit = input('Add your favorite fruit: ')
-# fruits.append(new_fruit)
-# print(fruits)
-# choose_fruit = int(input('Pick a number 1 through {}: '.format(len(fruits))))
-# print('You chose {}: {}'.format(choose_fruit, fruits[choose_fruit - 1]))
-# fruits = [input('Add another fruit: ')] + fruits
-# print(fruits)
-# fruits.insert(0, input('Add another fruit: '))
-# print(fruits)
-# for fruit in fruits:
-#     if fruit[0] == 'P':
-#         print(fruit)
-# print('=' * 40)
 
-# print('SERIES 2')
-# print('=' * 40)
-# print(fruits)
-# fruits.pop(-1)
-# print(fruits)
-# remove_fruit = input('Delete a fruit: ')
-# fruits.remove(remove_fruit)
-# print(fruits)
-# print('=' * 40)
+print('SERIES 1')
+print('=' * 40)
+print(fruits)
+new_fruit = input('Add your favorite fruit: ')
+fruits.append(new_fruit.capitalize())
+print(fruits)
+choose_fruit = int(input('Pick a number 1 through {}: '.format(len(fruits))))
+print('You chose {}: {}'.format(choose_fruit, fruits[choose_fruit - 1]))
+fruits = [input('Add another fruit: ')] + fruits
+print(fruits)
+fruits.insert(0, input('Add another fruit: '))
+print(fruits)
+for fruit in fruits:
+    if fruit[0] == 'P':
+        print(fruit)
+print('=' * 40)
+
+print('SERIES 2')
+print('=' * 40)
+print(fruits)
+fruits.pop(-1)
+print(fruits)
+remove_fruit = input('Delete a fruit: ')
+fruits.remove(remove_fruit.capitalize())
+print(fruits)
+print('=' * 40)
 
 print('SERIES 3')
 print('=' * 40)
 for fruit in fruits:
     answer = input('Do you like {}? '.format(fruit.lower()))
     while answer != 'yes' or 'no':
-        print('Please type "yes" or "no"')
-        answer = input('Do you like {}? '.format(fruit.lower()))
+        if answer == 'yes':
+            pass
+        elif answer == 'no':
+            fruits.remove(fruit)
+        else:
+            answer = input('Choose "yes" or "no" ')
+        break
+print(fruits)
+
